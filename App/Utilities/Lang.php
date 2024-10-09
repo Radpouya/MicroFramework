e245ef7a8911b5bb261e71a2f4a05a27d16c67f7
Mahdi <radpouyamahdi@gmail.com>
2024-10-09 22:00:12 +0330

    lang-utility-updated

--- App/Utilities/Lang.php
+++ App/Utilities/Lang.php
@@ -15,4 +15,8 @@ class Lang {
     $en_num = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
     return str_replace($fa_num, $en_num, (string)$input);
   }
+
+  public static function print_test($input) {
+    print_r($input)
+  }
 }
