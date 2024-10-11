4ba2adba82b411c1520bc00e1916c2590133c46f
Mahdi <radpouyamahdi@gmail.com>
2024-10-11 22:59:14 +0330

    Route Class implemented

--- App/Core/Request.php
+++ App/Core/Request.php
@@ -3,7 +3,48 @@
 namespace App\Core;
 
 class Request {
+  private $params;
+  private $method;
+  private $agent;
+  private $ip;
+  private $uri;
+
   public function __construct() {
-    echo "Request Class";
+    $this->params = $_REQUEST;
+    $this->method = $_SERVER['HTTP_USER_AGENT'];
+    $this->agent = $_SERVER['REQUEST_METHOD'];
+    $this->ip = $_SERVER['REMOTE_ADDR'];
+    $this->uri = strtok($_SERVER['REQUEST_URI'], '?');
+  }
+
+  public function __get(string $name) {
+    return $this->params[$name] ?? null;
+  }
+
+  public function method() {
+    return $this->method;
+  }
+
+  public function params() {
+    return $this->params;
+  }
+  public function agent() {
+    return $this->agent;
+  }
+  public function ip() {
+    return $this->ip;
+  }
+
+  public function input($key) {
+    return $this->params[$key] ?? null;
+  }
+
+  public function isset($key) {
+    return isset($this->params[$key]);
+  }
+
+  public function redirect($route) {
+    header("Location: " . site_url($route));
+    die();
   }
 }
--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 
-use App\Core\StupidRouter;
-
 include 'bootstrap/init.php';
-
-$router = new StupidRouter();
-$router->run();
-
